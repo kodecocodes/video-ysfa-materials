b7ff50ff4e41ee41d73f60fae33759c039fa5344
Brian Moakley <brian.douglas.moakley@gmail.com>
2021-08-17 16:03:00 -0400

    Adds sample projects for episode 5

---
+++ 05-make-a-network-call/final/rwcourses/lib/model/course.dart
@@ -0,0 +1,7 @@
+class Course {
+  final String courseId;
+  final String name;
+  final String description;
+
+  Course(this.courseId, this.name, this.description);
+}
---
+++ 05-make-a-network-call/starter/rwcourses/lib/rwcourses_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'strings.dart';
+
+class RWCoursesApp extends StatelessWidget {
+  const RWCoursesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.appTitle),
+      ),
+    );
+  }
+}

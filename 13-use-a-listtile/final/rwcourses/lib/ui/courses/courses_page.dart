9d123c1e36054caeb72b3075064d356e883ababd
Brian Moakley <brian.douglas.moakley@gmail.com>
2021-08-18 13:46:59 -0400

    Adds e13 sample projects

---
+++ 13-use-a-listtile/final/rwcourses/lib/ui/courses/courses_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:rwcourses/constants.dart';
+import 'courses_controller.dart';
+import '../../model/course.dart';
+import '../../repository/course_repository.dart';
+
+class CoursesPage extends StatefulWidget {
+  const CoursesPage({Key? key}) : super(key: key);
+
+  @override
+  _CoursesPageState createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> {
+  final _controller = CoursesController(CourseRepository());
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Course>>(
+        future: _controller.fetchCourses(Constants.allFilter),
+        builder: (context, snapshot) {
+          final courses = snapshot.data;
+          if (courses == null) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemCount: courses.length,
+              itemBuilder: (BuildContext context, int position) {
+                return _buildRow(courses[position]);
+              });
+        });
+  }
+
+  Widget _buildRow(Course course) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListTile(
+        title: Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Text(course.name, style: const TextStyle(fontSize: 18.0)),
+        ),
+        trailing: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.network(
+            course.artworkUrl,
+          ),
+        ),
+      ),
+    );
+  }
+}
